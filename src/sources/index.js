62b5bdc8cc2477ac138c33a30d5e070adabf22f4
Mondo <mondogao@gmail.com>
2017-06-11 14:59:58 +0800

    feat(sources): 增加与 api 对应方法

--- src/sources/index.js
+++ src/sources/index.js
@@ -3,118 +3,183 @@ import { normalize } from 'normalizr'
 import { commonFetchGet } from 'sources/utils'
 import { pictures, albums } from 'sources/schemas'
 
-export const getAlbumPictures = albumId => {
+// users
+export const postUsers = code => {
+  return Promise.resolve({
+    session: 'this is a fake session'
+  })
+}
+
+export const putUsers = (session, encryptedData) => {
+  return Promise.resolve()
+}
+
+// albums
+export const getAlbums = () => {
+  return Promise.resolve([
+    {
+      "id": "string",
+      "name": "string",
+      "desc": "string",
+      "coverSrc": "/assets/cover@2x.png",
+      "latelyUpdateTime": "string"
+    },
+    {
+      "id": "string",
+      "name": "string",
+      "desc": "string",
+      "coverSrc": "/assets/cover@2x.png",
+      "latelyUpdateTime": "string"
+    }
+  ])
+    .then(data => normalize(data, albums))
+}
+
+export const getAlbum = id => {
+  return Promise.resolve({
+    "id": "string",
+    "name": "string",
+    "desc": "string",
+    "coverSrc": "/statics/images/9j20-kdj9.png",
+    "latelyUpdateTime": "string"
+  })
+}
+
+export const getAlbumPictures = id => {
   return Promise.resolve([
     {
       id: '1',
       src: '/assets/cover@2x.png',
       desc: '我拍的照片和我本人一样苦苦的嘿嘿我拍的照片和我本人一样苦苦的嘿嘿我拍的照片和我本人一样苦苦的嘿嘿我拍的照片和我本人一样苦苦的嘿嘿',
-      date: '2014-05-20',
+      uploadDate: '2014-05-20',
       isLiked: false,
+      likeTimes: 0,
       user: {
         id: 1,
         nickname: 'SHINAN'
       },
       comments: [
         {
-          user: {
+          commenter: {
             id: 2,
             nickname: '麦冬',
           },
-          content: '还是本人比较帅'
+          content: '还是本人比较帅',
+          date: '2014-05-06'
         },
         {
-          user: {
+          commenter: {
             id: 2,
             nickname: '麦冬',
           },
-          content: '还是本人比较帅'
+          content: '还是本人比较帅',
+          date: '2014-05-06'
         },
         {
-          user: {
+          commenter: {
             id: 2,
             nickname: '麦冬',
           },
-          content: '还是本人比较帅'
+          content: '还是本人比较帅',
+          date: '2014-05-06'
         },
         {
-          user: {
+          commenter: {
             id: 2,
             nickname: '麦冬',
           },
-          content: '还是本人比较帅'
+          content: '还是本人比较帅',
+          date: '2014-05-06'
         },
         {
-          user: {
+          commenter: {
             id: 2,
             nickname: '麦冬',
           },
-          content: '还是本人比较帅'
+          content: '还是本人比较帅',
+          date: '2014-05-06'
         }
       ]
     },
-    {
-      id: '2',
-      src: '/assets/cover@2x.png',
-      desc: '我拍的照片和我本人一样苦苦的嘿嘿我拍的照片和我本人一样苦苦的嘿嘿我拍的照片和我本人一样苦苦的嘿嘿我拍的照片和我本人一样苦苦的嘿嘿',
-      date: '2014-05-20',
-      isLiked: true,
-      user: {
-        id: 1,
-        nickname: 'SHINAN'
-      },
-      comments: [
-        {
-          user: {
-            id: 2,
-            nickname: '麦冬',
-          },
-          content: '还是本人比较帅'
+      {
+        id: '2',
+        src: '/assets/cover@2x.png',
+        desc: '我拍的照片和我本人一样苦苦的嘿嘿我拍的照片和我本人一样苦苦的嘿嘿我拍的照片和我本人一样苦苦的嘿嘿我拍的照片和我本人一样苦苦的嘿嘿',
+        uploadDate: '2014-05-20',
+        isLiked: false,
+        likeTimes: 0,
+        user: {
+          id: 1,
+          nickname: 'SHINAN'
         },
-        {
-          user: {
-            id: 2,
-            nickname: '麦冬',
+        comments: [
+          {
+            commenter: {
+              id: 2,
+              nickname: '麦冬',
+            },
+            content: '还是本人比较帅',
+            date: '2014-05-06'
           },
-          content: '还是本人比较帅'
-        },
-        {
-          user: {
-            id: 2,
-            nickname: '麦冬',
+          {
+            commenter: {
+              id: 2,
+              nickname: '麦冬',
+            },
+            content: '还是本人比较帅',
+            date: '2014-05-06'
           },
-          content: '还是本人比较帅'
-        },
-        {
-          user: {
-            id: 2,
-            nickname: '麦冬',
+          {
+            commenter: {
+              id: 2,
+              nickname: '麦冬',
+            },
+            content: '还是本人比较帅',
+            date: '2014-05-06'
           },
-          content: '还是本人比较帅'
-        },
-        {
-          user: {
-            id: 2,
-            nickname: '麦冬',
+          {
+            commenter: {
+              id: 2,
+              nickname: '麦冬',
+            },
+            content: '还是本人比较帅',
+            date: '2014-05-06'
           },
-          content: '还是本人比较帅'
-        },
-        {
-          user: {
-            id: 2,
-            nickname: '麦冬',
+          {
+            commenter: {
+              id: 2,
+              nickname: '麦冬',
+            },
+            content: '还是本人比较帅',
+            date: '2014-05-06'
           },
-          content: '还是本人比较帅'
-        },
-        {
-          user: {
-            id: 2,
-            nickname: '麦冬',
+          {
+            commenter: {
+              id: 2,
+              nickname: '麦冬',
+            },
+            content: '还是本人比较帅',
+            date: '2014-05-06'
           },
-          content: '还是本人比较帅'
-        }
-      ]
-    }
+          {
+            commenter: {
+              id: 2,
+              nickname: '麦冬',
+            },
+            content: '还是本人比较帅',
+            date: '2014-05-06'
+          }
+        ]
+      }
   ])
     .then(data => normalize(data, pictures))
+}
+
+// pictures
+export const postPictureComments = (id, session, content) => {
+  return Promise.resolve()
+}
+
+export const postPictureLikes = (id, session) => {
+  return Promise.resolve()
 }
